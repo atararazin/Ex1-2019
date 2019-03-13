d3f96fabd0d823d2b9dcb6b9aa67d18700a8dbb1
Atara Razin <atararazin@gmail.com>
2019-03-13 11:18:30 +0200

    added compsedmission and FunctionsContainer

---
+++ Ex1-2019/ComposedMission.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public class ComposedMission : IMission
+    {
+        private string name;
+        public String Name
+        {
+            get { return name; }
+        }
+
+        private string type;
+        public String Type
+        {
+            get { return type; }
+        }
+        public event EventHandler<double> OnCalculate;
+        private IMission m;
+
+        public ComposedMission(string name)
+        {
+            this.name = name;
+            this.type = "Composed";
+        }
+
+        public double Calculate(double value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ComposedMission Add(Mission missionToAdd)
+        {
+            if (this.m == null)
+            {
+                this.m = new SingleMission(missionToAdd, this.name);
+            }
+            else
+            {
+                //add to list.
+            }
+            return this;
+        }
+    }
+}
---
+++ Ex1-2019/MissionContainer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public delegate double Mission(double x);
+
+    public class FunctionsContainer
+    {
+        Mission functionsDelegate;
+        public Dictionary<string, Mission> funcNames = new Dictionary<string, Mission>();
+        public Mission this[string index]
+        {
+            get
+            {
+                return funcNames[index];
+            }
+            set
+            {
+                functionsDelegate += value;
+                funcNames.Add(index, value);
+            }
+        }
+
+        public List<string> getAllMissions()
+        {
+            return new List<string>(this.funcNames.Keys);
+        }
+    }
+}
