50e1f65bf74fdad9e93ae8e2a4f820059d2cc1b6
Atara Razin <atararazin@gmail.com>
2019-03-13 11:15:43 +0200

    added singleMission

---
+++ Ex1-2019/SingleMission.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    public class SingleMission : IMission
+    {
+        private string name;
+        public String Name
+        {
+            get { return name; }
+        }
+
+        private string type;
+        public String Type
+        {
+            get { return type; }
+        }
+
+        public event EventHandler<double> OnCalculate;
+        private Mission mission;
+
+        //constructor
+        public SingleMission(Mission mission, String Name)
+        {
+            this.mission = mission;
+            this.name = Name;
+            this.type = "Single";
+        }
+
+
+        public double Calculate(double value)
+        {
+            return this.mission.Invoke(value);
+        }
+    }
+}
